fab521ad2d14a5e31afbb919e7427b76ff1e67ca
Chalermchai Wauwai <59055693+phetchalermchai@users.noreply.github.com>
2024-06-23 00:08:14 +0700

    add navbar & content

---
+++ src/script/action.js
@@ -0,0 +1,144 @@
+
+// event toggle menu mobile
+const navbar = document.querySelector(".container-nav")
+const hamburgerMenu = document.querySelector(".container-nav .navbar .nav-hamburger img")
+const hiddenScroll = document.body
+const circleLogo = document.querySelector(".container-nav .navbar .nav-logo .circle")
+hamburgerMenu.addEventListener("click",()=>{
+    if (navbar.className === "container-nav") {
+        navbar.className = "container-nav open"
+        hiddenScroll.style.overflowY = "hidden"
+        hamburgerMenu.src = "src/img/mobile/ph_x.svg"
+
+        circleLogo.style.background = ""
+
+    } else {
+        navbar.className = "container-nav"
+        hamburgerMenu.src = "src/img/mobile/hamburger_black.svg"
+        hiddenScroll.style.overflowY = "auto"
+        if (document.documentElement.scrollTop >= 70) {
+            hamburgerMenu.src = "src/img/mobile/hamburger-white.svg"
+
+            circleLogo.style.background = "#FFFFFF"
+        }
+    }
+})
+
+const mbLogo = document.querySelector(".container-nav .navbar .nav-logo .logo-mobile .open")
+mbLogo.addEventListener("click",()=>{
+    navbar.className = "container-nav"
+    hamburgerMenu.src = "src/img/mobile/hamburger_black.svg"
+    hiddenScroll.style.overflowY = "auto"
+    if (document.documentElement.scrollTop >= 70) {
+        hamburgerMenu.src = "src/img/mobile/hamburger-white.svg"
+        circleLogo.style.background = "#FFFFFF"
+    }
+})
+
+const mbLink = document.querySelectorAll(".container-nav .nav-menu .menu-mobile")
+for (const iterator of mbLink) {
+    iterator.addEventListener("click",()=>{
+        navbar.className = "container-nav"
+        hamburgerMenu.src = "src/img/mobile/hamburger_black.svg"
+        hiddenScroll.style.overflowY = "auto"
+        if (document.documentElement.scrollTop >= 70) {
+            hamburgerMenu.src = "src/img/mobile/hamburger-white.svg"
+
+            circleLogo.style.background = "#FFFFFF"
+        }
+    })
+}
+
+// event scroll navbar
+window.onscroll = function() {
+    const scroll = document.documentElement.scrollTop
+    const mbLogo = document.querySelector(".container-nav .navbar .nav-logo .logo-mobile .close")
+    const Logo = document.querySelector(".container-nav .navbar .nav-logo .logo a")
+    const Link = document.querySelectorAll(".container-nav .nav-menu .menu")
+
+    if (scroll >=70) {
+        navbar.style.background = "#285E67"
+        hamburgerMenu.src = "src/img/mobile/hamburger-white.svg"
+
+        // circlelogo
+        circleLogo.style.background = "#FFFFFF"
+        circleLogo.addEventListener("mouseover",()=>{
+            circleLogo.style.background = "#EBBFAE"
+        })
+        circleLogo.addEventListener("mouseout",()=>{
+            circleLogo.style.background = "#FFFFFF"
+        })
+
+        // menu desktop
+        for (const iterator of Link) {
+            iterator.style.color = "#FFFFFF"
+            iterator.addEventListener('mouseover',()=>{
+                iterator.style.color = "#EBBFAE"
+            })
+            iterator.addEventListener('mouseout',()=>{
+                iterator.style.color = "#FFFFFF"
+            })
+        }
+
+        // logo mobile
+        mbLogo.style.color = "#FFFFFF"
+        mbLogo.addEventListener("mouseover",()=>{
+            mbLogo.style.color = "#EBBFAE"
+            mbLogo.style.transition = "1s"
+        })
+        mbLogo.addEventListener("mouseout",()=>{
+            mbLogo.style.color = "#FFFFFF"
+        })
+
+        // logo desktop
+        Logo.style.color = "#FFFFFF"
+        Logo.addEventListener('mouseover',()=>{
+            Logo.style.color = "#EBBFAE"
+        })
+        Logo.addEventListener('mouseout',()=>{
+            Logo.style.color = "#FFFFFF"
+        })
+    }else{
+        navbar.style.background = ""
+        hamburgerMenu.src = "src/img/mobile/hamburger_black.svg"
+
+        // menudesktop
+        for (const iterator of Link) {
+            iterator.style.color = ""
+            iterator.addEventListener('mouseover',()=>{
+                iterator.style.color = "#EBBFAE"
+            })
+            iterator.addEventListener('mouseout',()=>{
+                iterator.style.color = ""
+            })
+        }
+
+        // circlelogo
+        circleLogo.style.background = ""
+        circleLogo.addEventListener("mouseover",()=>{
+            circleLogo.style.background = "#EBBFAE"
+        })
+        circleLogo.addEventListener("mouseout",()=>{
+            circleLogo.style.background = ""
+        })
+
+        // logo mobile
+        mbLogo.style.color = ""
+        mbLogo.addEventListener("mouseover",()=>{
+            mbLogo.style.color = "#EBBFAE"
+            mbLogo.style.transition = "1s"
+        })
+        mbLogo.addEventListener("mouseout",()=>{
+            mbLogo.style.color = ""
+        })
+
+        // logo desktop
+        Logo.style.color = ""
+        Logo.addEventListener('mouseover',()=>{
+            Logo.style.color = "#EBBFAE"
+        })
+        Logo.addEventListener('mouseout',()=>{
+            Logo.style.color = ""
+        })
+    }
+}
